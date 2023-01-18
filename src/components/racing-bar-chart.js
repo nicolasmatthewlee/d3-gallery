6ed1a0eb77fa86bae7c6dc131bd81250a0fdd505
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-17 16:04:27 -0800

    added play/pause button whose button text is dependent on state.play

--- src/components/racing-bar-chart.js
+++ src/components/racing-bar-chart.js
@@ -1,22 +1,73 @@
 import { useRef, useEffect, useState } from "react";
+import { scaleBand, scaleLinear, select } from "d3";
 
 export const RacingBarChart = () => {
-  const [data, setData] = useState(Array(5).fill(0));
+  const [data, setData] = useState([
+    { color: "red", value: 1 },
+    { color: "blue", value: 2 },
+    { color: "green", value: 3 },
+    { color: "orange", value: 4 },
+    { color: "purple", value: 5 },
+  ]);
+
+  const [play, setPlay] = useState(false);
+
+  const svgRef = useRef();
 
   // update data with setInterval
   useEffect(() => {
     const interval = setInterval(() => {
-      setData((data) =>
-        data.map((n) => Math.floor(n + Math.random() * 3 + 1))
-      );
-    }, 500);
+      if (play) {
+        setData((data) =>
+          data.map((obj) => ({
+            color: obj.color,
+            value: (obj.value += Math.floor(Math.random() * 5 + 1)),
+          }))
+        );
+      }
+    }, 100);
     return () => clearInterval(interval);
   });
 
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+    // sort the data
+    data.sort((a, b) => b.value - a.value);
+
+    // create x-scale, y-scale
+    const yScale = scaleBand()
+      .domain(data.map((n, i) => i))
+      .range([0, 150])
+      .padding(0.3);
+
+    const xScale = scaleLinear()
+      .domain([0, Math.max(...data.map((obj) => obj.value))])
+      .range([0, 300]);
+
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .attr("x", 0)
+      .attr("y", (n, i) => yScale(i))
+      .attr("width", (n) => xScale(n.value))
+      .attr("height", yScale.bandwidth())
+      .style("fill", (n) => n.color);
+  }, [data]);
+
   return (
     <div>
-      <h1>{data.map((n) => n + " ")}</h1>
-      <svg className="chart"></svg>
+      <h1>{data.map((n) => n.value + " ")}</h1>
+      <svg ref={svgRef} className="chart"></svg>
+      <button
+        onClick={(e) => {
+          setPlay(!play);
+        }}
+      >
+        {play ? "Pause" : "Play"}
+      </button>
     </div>
   );
 };
